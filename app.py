9ef50b3f7275bd1735f50538a07087767c95b61c
Ronak_Suthar <ronakronu02@gmail.com>
2023-05-10 19:38:00 +0530

    resolved db

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from flask import Flask,render_template,request,redirect,url_for,send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail,Message
 from config import mail_username,mail_password
+
 app=Flask(__name__)
+
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT']=465
 app.config['MAIL_USERNAME']=mail_username
 app.config['MAIL_PASSWORD']=mail_password
 app.config['MAIL_USE_TLS']=False
 app.config['MAIL_USE_SSL']=True
-
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///userdata.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
